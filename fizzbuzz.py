e706eb9f17831d873a3c07b336b4ee64f34d6480
bishal_LD <bishal.magar@thamescollege.edu.np>
2020-02-17 16:59:30 +0545

    adding

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+import sys 
+
+def fizzbuzz():
+    for i in range(1,1+n):
+        if i%3==0:print('fizz')
+        elif i%5==0:print('buzz')
+        elif i%3 and i%5==0:print('fizzbuzz')
+
+if __name__=='__main__'
+    fizzbuzz(int(sys.a))
+
+
